ffebae7f0a48408ef7b0b1663b1bcf135fa05b95
yanxia <yanxia0414@gmail.com>
2018-04-02 08:23:31 +0000

    update

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,18 +1,20 @@
 <?php
-function get_db_config(){
-    if(getenv('IS_IN_HEROKU')){
-        $url=parse_url(getenv("DATABASE_URL"));
 
-        return $db_config=[
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
             'connection' => 'pgsql',
             'host' => $url["host"],
-            'database' => substr($url["path"], 1),
+            'database'  => substr($url["path"], 1),
             'username'  => $url["user"],
             'password'  => $url["pass"],
         ];
-    }else{
-        return $db_config=[
-            'connection'=>env('DB_CONNECTION', 'mysql'),
+    } else {
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
             'host' => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
@@ -20,7 +22,3 @@ function get_db_config(){
         ];
     }
 }
-
-
-
- ?>
